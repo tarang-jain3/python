cff441dc379e0501af72069a5cb7ba9d945a52a8
Tarang jain <143580109+tarang-jain3@users.noreply.github.com>
2024-09-18 23:40:37 +0530

    Add files via upload

---
+++ currency converter dollar to ruppes.py
@@ -0,0 +1,6 @@
+# WAP for currency converter dollar to ruppes
+
+print(" Convert dollar into ruppes")
+dollarAmount = int(input("Enter the amount in Dollar. : "))
+dollarIntoRs = (dollarAmount * 84)
+print(dollarAmount, " convert into ruppes ", dollarIntoRs)
---
+++ currency converter of Rs. to$.py
@@ -0,0 +1,5 @@
+# WAP for currency converter of ruppes to dollar
+print(" Convert ruppes into dollars")
+ruppesAmount = int(input("Enter the amount in Rs. : "))
+rsIntoDollar = (ruppesAmount / 84)
+print(ruppesAmount, " convert into dollar ", rsIntoDollar)
