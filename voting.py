f7bfab84d0c3515fd6aa538c6422b7c8aa8dcf34
Tarang jain <143580109+tarang-jain3@users.noreply.github.com>
2024-09-18 23:42:56 +0530

    Add files via upload

---
+++ voting.py
@@ -0,0 +1,7 @@
+#WAP to check that you are eligible for voting or not
+userAge = int(input("Enter your age"))
+#for voting you must greater that 17
+if(userAge > 17):
+    print("Yor are eligible for voting")
+else:
+    print("Yor have to wait")
